aef3d9c02d024827520fa5e8e93505d217b7f0f4
rangercurtis <38321554+rangercurtis@users.noreply.github.com>
2021-09-15 17:22:31 -0500

    Add files via upload

---
+++ Curtis_Portfolio.js
@@ -0,0 +1,27 @@
+﻿//Curtis_Portfolio Script File
+
+//Load React Like Button:
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+    render() {
+        if (this.state.liked) {
+            return '<yellow>All Right! You liked this.<yellow>';
+        }
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
---
+++ QuiltBindingStrips.js
@@ -0,0 +1,10 @@
+﻿function myFunction() {
+
+    let perimeter = 0;
+    let length1 = document.getElementById("length").value;
+    let width1 = document.getElementById("width").value;
+    perimeter = 2 * (length1 + width1);
+    stripCount = (perimeter / 42);
+}
+
+document.getElementById('strip').innerHTML = stripCount;
